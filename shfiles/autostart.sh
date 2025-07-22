24af7d7cba3eb1dea7dd690ac88091b236e2af72
Songyan Liu <liusongyan04@qq.com>
2025-07-22 22:18:01 +0800

    更新自启动脚本

--- shfiles/autostart.sh
+++ shfiles/autostart.sh
@@ -7,10 +7,34 @@ PUBLIC="source /opt/ros/noetic/setup.bash && \
 
 sleep 5s
 {
+    gnome-terminal --title "FAST-LIO2" -- bash -c \
+   "$PUBLIC && \
+    roslaunch fast_lio mapping_mid360.launch; \
+    exec bash"
+}&
+
+sleep 5s
+{
+    gnome-terminal --title "odometry_viewr" -- bash -c \
+   "$PUBLIC && \
+    rostopic echo /Odom_high_freq; \
+    exec bash"
+}&
+
+sleep 2s
+{
     gnome-terminal --title "PX4 Start" -- bash -c \
    "$PUBLIC && \
     sudo chmod 777 /dev/ttyACM0 && \
     roslaunch mavros px4.launch; \
+    exec bash"
+}&
+
+sleep 5s
+{
+    gnome-terminal --title "PX4 Control" -- bash -c \
+   "$PUBLIC && \
+    roslaunch px4ctrl run_ctrl.launch; \
     exec bash"
 }&
 
